cc07bced73576e3132b65a28410982782f65f6a8
Yiyang Zhang <77761352+yiyangzhang2020@users.noreply.github.com>
2021-03-11 11:55:03 -0500

    Update ehr_analysis_part3.py

--- ehr_analysis_part3.py
+++ ehr_analysis_part3.py
@@ -11,68 +11,73 @@ import matplotlib as plt
   * instance attributes for value, units, etc.
   '''
 
-class Patients():
-    #define data entry
-    def __init__(self,entry):
-        self.id = entry[0]
-        self.gender = entry[1]
-        self.birth_date = datetime.strptime(entry[2], '%Y-%m-%d %H:%M:%S.%f')
-        self.race = entry[3]
-        self.MS = entry[4]
-        self.language = entry[5]
-        self.population_underproverty = entry[6]
+def load_patients(patients_file):
+	with open(patients_file) as patient_table:
+		return [line.replace('\n','').split('\t') for line in patient_table][1:]
 
-    #instance attributes
-    def __int__(self,id,gender,birth_date, race,MS,language,population_underproverty):
-        self.id=id
-        self.gender=gender
-        self.birth_date=birth_date
-        self.race=race
-        self.MS=MS
-        self.language=language
-        self.population_underproverty=population_underproverty
+def load_labs(labs_file):
+	with open(labs_file) as labs_table:
+		return [line.replace('\n','').split('\t') for line in labs_table][1:]
 
-    #read in converted data from original file
-    def readlab(self, labs_file):
-        with open(labs_file) as labs_table:
-            labfile = [line.replace('\n', '').split('\t') for line in labs_table]
-        sickness = [row for row in labfile if row[0] == self.id]
-        self.sickness = sickness
+class Patient():
 
-    def getsick(self):
-        return self.sickness
+    global patients, labs
+    patients = load_patients('/Users/yiyangzhang/Desktop/Biostat821/ehr_analysis/PatientCorePopulatedTable.txt')
+    labs = load_labs('/Users/yiyangzhang/Desktop/Biostat821/ehr_analysis/LabsCorePopulatedTable.txt')
 
-    def getid(self):
-        return self.id
-
-    def getage(self):
-        num = round((datetime.now() - self.birth_date).days / 365, 1)
-        return num
-
-    def getgender(self):
-        return self.gender
-
-    def getrace(self):
-        return self.race
-
+    def __init__(self, patientID):
+        for row in patients:
+            if row[0] == patientID:
+                self.id = row[0]
+                self.gender = row[1]
+                self.dob = datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S.%f')
+                self.race = row[3]
+                self.marital = row[4]
+                self.language = row[5]
+                self.poverty = row[6]
+   
+    @property
+    def age(self):
+        return round((datetime.now() - self.dob).days / 365, 1)
+    
     def __lt__(self, other):
-        if type(other) == int or type(other) == float:
+        if isinstance(other, float):
             return self.age < other
-        else:
+        if isinstance(other, Patient):
             return self.age < other.age
 
     def __gt__(self, other):
-        if type(other) == int or type(other) == float:
+        if isinstance(other, float):
             return self.age > other
-        else:
+        if isinstance(other, Patient):
             return self.age > other.age
 
-    def plot(self, sick):
-        values = [row[3] for row in self.sickness if row[2] == sick]
-        # yr = [row[5][:4] for row in self.sickness if row[2]==sick]
-        plt.plot(values)
-        plt.title(self.id + '\n' + sick)
-        plt.show()
+    def plot(self, labname, filename):
+        lab_datetime = []
+        lab_value = []
+        for row in labs:
+            if self.id == row[0] and labname == row[2]:
+               lab_value.append(row[3])
+               lab_datetime.append(datetime.strptime(row[5], '%Y-%m-%d %H:%M:%S.%f'))
+            lab_unit = row[4]
+        plt.plot(lab_datetime, lab_value)
+        plt.xlabel('Lab datetime')
+        plt.ylabel(f'Lab value / {lab_unit}')
+        plt.title(f'{self.id}// {labname}')
+        plt.savefig(filename)
 
-    ID = property(getid)
-    age = property(getage)
+class Observation():
+    global labs
+    labs = load_labs('/Users/MineMine/Desktop/X0029/LabsCorePopulatedTable.txt')
+    def __init__(self, patientID, labname, labdatetime):
+        for row in labs:
+            if patientID == row[0] and labname == row[2] and labdatetime == datetime.strptime(row[5], '%Y-%m-%d %H:%M:%S.%f'):
+                self.id = row[0]
+                self.labname = row[2]
+                self.labvalue = row[3]
+                self.labunit = row[4]
+                self.labdatetime = datetime.strptime(row[5], '%Y-%m-%d %H:%M:%S.%f')
+
+if __name__ == '__main__':
+	print(Patient('DB22A4D9-7E4D-485C-916A-9CD1386507FB') > 57.0)
+	Patient('DB22A4D9-7E4D-485C-916A-9CD1386507FB').plot("URINALYSIS: PH", "ph_over_time.png")
